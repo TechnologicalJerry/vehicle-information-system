0f24629a731927c8f87b006ba9a68c9c2527889e
Rajat Verma <cserajatverma@gmail.com>
2026-08-01 22:40:22 +0530

    feat: create UserServiceModule with configuration, middleware, and global exception handling

---
+++ nest-server/apps/user-service/src/user-service.module.ts
@@ -0,0 +1,33 @@
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppConfigModule } from '@app/config';
+import { LoggerModule, CorrelationIdMiddleware } from '@app/logger';
+import { UsersModule } from './users/users.module';
+import { GlobalExceptionFilter } from '../../api-gateway/src/filters/global-exception.filter';
+
+@Module({
+  imports: [AppConfigModule, LoggerModule, UsersModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
+})
+export class UserServiceModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorrelationIdMiddleware).forRoutes('*');
+  }
+}

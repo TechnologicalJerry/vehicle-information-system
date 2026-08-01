525b3d881db400e63f5cbc42e271f1e2e3db24af
Rajat Verma <cserajatverma@gmail.com>
2026-08-01 22:41:09 +0530

    feat: create VehicleServiceModule with configuration, middleware, and global exception handling

---
+++ nest-server/apps/vehicle-service/src/vehicle-service.module.ts
@@ -0,0 +1,33 @@
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppConfigModule } from '@app/config';
+import { LoggerModule, CorrelationIdMiddleware } from '@app/logger';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { GlobalExceptionFilter } from '../../api-gateway/src/filters/global-exception.filter';
+
+@Module({
+  imports: [AppConfigModule, LoggerModule, VehiclesModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
+})
+export class VehicleServiceModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorrelationIdMiddleware).forRoutes('*');
+  }
+}

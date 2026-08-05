7ecbd7315edb89857de6297002eb30536da02875
Rajat Verma <cserajatverma@gmail.com>
2026-08-05 09:15:20 +0530

    feat: create index file for testing library to export mock builders and test utilities

---
+++ nest-server/libs/testing/src/index.ts
@@ -0,0 +1,5 @@
+export * from './mock-builders/prisma.mock';
+export * from './mock-builders/redis.mock';
+export * from './mock-builders/kafka.mock';
+export * from './mock-builders/mqtt.mock';
+export * from './test-utilities';
